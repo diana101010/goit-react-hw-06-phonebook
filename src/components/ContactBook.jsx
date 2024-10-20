99b93df3c9f658d4755e52bd10835865f523af73
diana101010 <150705147+diana101010@users.noreply.github.com>
2024-10-20 18:47:03 +0300

    5

--- src/components/ContactBook.jsx
+++ src/components/ContactBook.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact, deleteContact, setFilter } from './contactsSlice';
-import { store, persistor } from './store'; 
+//import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const ContactBook = () => {
